1769070b399227571c0a319a3e0217fa7a419aa2
mrshawnh <hen23020@byui.edu>
2023-09-14 10:57:41 -0700

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int grade_int = int.Parse(grade);
+
+        string letter = "";
+
+        if (grade_int >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_int >= 80 && grade_int < 90)
+        {
+            letter = "B";
+        }
+        else if (grade_int >= 70 && grade_int < 80)
+        {
+            letter = "C";
+        }
+        else if (grade_int >= 60 && grade_int < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade Letter: {letter}");
+
+        if (grade_int >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
