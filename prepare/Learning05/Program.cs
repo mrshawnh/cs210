20f325c9451bd3e964ce6b95b1b02a8413c957e2
mrshawnh <hen23020@byui.edu>
2023-11-09 04:26:07 -0700

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Red", 5, 4);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Yellow", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes){
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The color of the shape is {color}, and the area is {area}.");
+        }
+
     }
 }
